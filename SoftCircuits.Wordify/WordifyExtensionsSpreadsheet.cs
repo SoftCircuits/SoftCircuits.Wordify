f7c97823ca924651142a0fb8e5b3176c41537931
Jonathan Wood <jwood@softcircuits.com>
2022-06-11 08:53:04 -0600

    Added support for spreadsheet column names.

---
+++ SoftCircuits.Wordify/WordifyExtensionsSpreadsheet.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) 2022 Jonathan Wood (www.softcircuits.com)
+// Licensed under the MIT license.
+//
+
+using System.Text;
+
+namespace SoftCircuits.Wordify
+{
+    public static partial class WordifyExtensions
+    {
+        public const string DefaultColumnName = "A";
+        private const int AlphabetLength = 26;
+
+        /// <summary>
+        /// Converts an integer value to a spreadsheet column name. (For example, 2 would be converted
+        /// to "B" and 28 would be converted to "AB".)
+        /// </summary>
+        /// <param name="column">The column number to convert.</param>
+        /// <returns>The converted column name, or "A" if <paramref name="column"/> does not represent
+        /// a valid spreadsheet column number.</returns>
+        public static string ToSpreadsheetColumn(this int column)
+        {
+            if (column > 0)
+            {
+                StringBuilder builder = new(6);
+
+                do
+                {
+                    column--;
+                    char c = (char)('A' + column % AlphabetLength);
+                    builder.Insert(0, c);
+                    column /= AlphabetLength;
+                } while (column > 0);
+
+                return builder.ToString();
+            }
+            return DefaultColumnName;
+        }
+
+        /// <summary>
+        /// Parses a spreadsheet column name and returns its corresponding integer value.
+        /// </summary>
+        /// <param name="s">The column name to parse.</param>
+        /// <returns>The corresponding integer column value.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static int ParseSpreadsheetColumn(this string? s)
+        {
+            if (!TryParseSpreadsheetColumn(s, out int result))
+                throw new ArgumentException("Invalid spreadsheet column format", nameof(s));
+            return result;
+        }
+
+        /// <summary>
+        /// Attempts to parse a spreadsheet column name and return its corresponding integer value.
+        /// </summary>
+        /// <param name="s">The column name to parse.</param>
+        /// <param name="result">If successful, is set to the parsed integer value.</param>
+        /// <returns>True if successful, false if <paramref name="s"/> could not be parsed.</returns>
+        public static bool TryParseSpreadsheetColumn(this string? s, out int result)
+        {
+            int pos = 0;
+            result = 0;
+
+            if (s != null)
+            {
+                // Skip leading whitespace
+                while (pos < s.Length && !char.IsLetter(s[pos]))
+                    pos++;
+
+                if (pos < s.Length)
+                {
+                    do
+                    {
+                        int i = char.ToUpper(s[pos]) - 'A';
+                        result *= AlphabetLength;
+                        result += i + 1;
+                        pos++;
+
+                    } while (pos < s.Length && char.IsLetter(s[pos]));
+
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ WordifyTests/TestSpreadsheet.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2022 Jonathan Wood (www.softcircuits.com)
+// Licensed under the MIT license.
+//
+
+using SoftCircuits.Wordify;
+
+namespace WordifyTests
+{
+    [TestClass]
+    public class TestSpreadsheet
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual("A", (-1).ToSpreadsheetColumn());
+            Assert.AreEqual("A", 0.ToSpreadsheetColumn());
+            Assert.AreEqual("A", 1.ToSpreadsheetColumn());
+            Assert.AreEqual("B", 2.ToSpreadsheetColumn());
+            Assert.AreEqual("C", 3.ToSpreadsheetColumn());
+            Assert.AreEqual("Z", 26.ToSpreadsheetColumn());
+            Assert.AreEqual("AA", 27.ToSpreadsheetColumn());
+            Assert.AreEqual("AB", 28.ToSpreadsheetColumn());
+
+            Assert.AreEqual("DKJ", 3000.ToSpreadsheetColumn());
+            Assert.AreEqual("HVT", 6000.ToSpreadsheetColumn());
+            Assert.AreEqual("ECCN", 90000.ToSpreadsheetColumn());
+        }
+
+        [TestMethod]
+        public void TestParse()
+        {
+            Assert.IsFalse(((string)null!).TryParseSpreadsheetColumn(out int _));
+            Assert.IsFalse(string.Empty.TryParseSpreadsheetColumn(out int _));
+            Assert.IsFalse("@@@".TryParseSpreadsheetColumn(out int _));
+
+            Assert.AreEqual(1, "A".ParseSpreadsheetColumn());
+            Assert.AreEqual(2, "B".ParseSpreadsheetColumn());
+            Assert.AreEqual(3, " C ".ParseSpreadsheetColumn());
+            Assert.AreEqual(26, " z ".ParseSpreadsheetColumn());
+            Assert.AreEqual(27, " aa ".ParseSpreadsheetColumn());
+            Assert.AreEqual(28, "ab".ParseSpreadsheetColumn());
+
+            Assert.AreEqual(3000, " dkj ".ParseSpreadsheetColumn());
+            Assert.AreEqual(6000, "HVT".ParseSpreadsheetColumn());
+            Assert.AreEqual(90000, "eccn".ParseSpreadsheetColumn());
+        }
+    }
+}
